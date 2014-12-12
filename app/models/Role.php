9ba521343f1244c41116b47ff8cab8d84f1e8dde
akaramires <e.abdurayimov@gmail.com>
2014-12-13 00:33:00 +0300

    migrations

---
+++ app/models/Role.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by Elmar <aka Ramires> Abdurayimov <e.abdurayimov@gmail.com>
+ * @copyright (C)Copyright 2014 eatech.org
+ * Date: 12/13/14
+ * Time: 1:22 AM
+ */
+
+use Zizaco\Entrust\EntrustRole;
+
+class Role extends EntrustRole
+{
+}
--- app/routes.php
+++ app/routes.php
@@ -11,7 +11,38 @@
 |
 */
 
-Route::get('/', function()
-{
-	return View::make('hello');
+Route::get('/', function () {
+    return View::make('hello');
+});
+
+Route::get('/start', function () {
+    $subscriber       = new Role();
+    $subscriber->name = 'Subscriber';
+    $subscriber->save();
+
+    $author       = new Role();
+    $author->name = 'Author';
+    $author->save();
+
+    $read               = new Permission();
+    $read->name         = 'can_read';
+    $read->display_name = 'Can Read Posts';
+    $read->save();
+
+    $edit               = new Permission();
+    $edit->name         = 'can_edit';
+    $edit->display_name = 'Can Edit Posts';
+    $edit->save();
+
+    $subscriber->attachPermission($read);
+    $author->attachPermission($read);
+    $author->attachPermission($edit);
+
+    $user1 = User::find(1);
+    $user2 = User::find(2);
+
+    $user1->attachRole($subscriber);
+    $user2->attachRole($author);
+
+    return 'Woohoo!';
 });
